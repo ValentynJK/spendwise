80d5cd2d70767f813d42aba1fef7ce6817573405
valentynJK <valentynejk@gmail.com>
2026-01-29 18:53:18 +0100

    initial setup for docker in dev

--- apps/mobile-web/vite.config.ts
+++ apps/mobile-web/vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true, // Needed for Docker to expose the port
+    port: 5173,
+    watch: {
+      usePolling: true, // Use this if HMR doesn't work on Windows/Mac
+    },
+  },
   plugins: [react()],
 })
